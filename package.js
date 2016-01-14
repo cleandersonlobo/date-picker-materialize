ee76944e5afec2878f76f1e966f3e5fda3445e73
Cleanderson Lobo <cleandersonlobo@gmail.com>
2016-01-14 14:38:27 -0300

    update picker.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'cleandersonlobo:date-picker-materialize',
-  version: '0.1.1',
+  version: '0.1.3',
   // Brief, one-line summary of the package.
   summary: 'Test version only the Datepicker the Materialize package.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cleandersonlobo/date-picker-materialize.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
